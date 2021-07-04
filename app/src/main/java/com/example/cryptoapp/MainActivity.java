2b3ce6c76b405db65ab7c6a6870838ae513be744
edwinaringo <tedaringo628@gmail.com>
2021-07-04 19:35:22 +0300

    Adding an OnClick listner

--- app/src/main/java/com/example/cryptoapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.cryptoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindCryptosButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindCryptosButton = (Button)findViewById(R.id.findCryptosButton);
+            mFindCryptosButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //do something
+                }
+            });
     }
 }
