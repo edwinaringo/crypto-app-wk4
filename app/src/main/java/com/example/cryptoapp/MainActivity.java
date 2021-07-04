b2009a41b26b08eb4ffcd39802c08d712837e45f
edwinaringo <tedaringo628@gmail.com>
2021-07-04 22:06:45 +0300

    Added navigation to the Crypto Activity

--- app/src/main/java/com/example/cryptoapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cryptoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
             mFindCryptosButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //do something
+                    Intent intent = new Intent(MainActivity.this, CryptoActivity.class);
+                    startActivity(intent);
                 }
             });
     }
