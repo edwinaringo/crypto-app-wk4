e1274bac43040466118aa71737de7961feece7b6
edwinaringo <tedaringo628@gmail.com>
2021-07-11 17:16:37 +0300

    Initializing variables to hold the views in the ListActivity class

--- app/src/main/java/com/example/cryptoapp/ListActivity.java
+++ app/src/main/java/com/example/cryptoapp/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.cryptoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,42 +9,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 
 public class ListActivity extends AppCompatActivity {
+    private EditText searchEdit;
+    private RecyclerView currenciesRV;
+    private ProgressBar loadingPB;
 
-
-    @BindView(R.id.nameTextView) TextView mNameTextView;
-    @BindView(R.id.listView) ListView mListView;
-
-
-    private String[] cryptos = new String[] {
-            "Bitcoin (BTC)", "Ethereum (ETH)", "Tether (USDT)", "Litecoin (LTC)","Cardano (ADA)", "Dogecoin (DOGE)","Polkadot (DOT)","Bitcoin Cash (BCH)", "Stellar (XLM)","Chainlink","Binance Coin (BNB)","Solana (SOL)", "Polygon (MATIC)"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        searchEdit = findViewById(R.id.EditSearch);
+        currenciesRV = findViewById(R.id.idRVCurrencies);
+        loadingPB = findViewById(R.id.idPBLoading);
 
-        mListView = (ListView) findViewById(R.id.listView);
-        mNameTextView = (TextView) findViewById(R.id.nameTextView);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cryptos);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(ListActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        mNameTextView.setText("Here is a list of cryptocurrencies for " + name);
     }
 }
