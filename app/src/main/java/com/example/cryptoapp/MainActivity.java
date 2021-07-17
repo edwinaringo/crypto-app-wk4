e1ebc8eb020adf388931bad693be71790c86b4fb
edwinaringo <tedaringo628@gmail.com>
2021-07-17 13:11:56 +0300

    Editing the logic on the main activity to 'accept' the new layout

--- app/src/main/java/com/example/cryptoapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoapp/MainActivity.java
@@ -8,25 +8,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-
-
-   // @BindView(R.id.findCryptosButton) Button mFindCryptosButton;
-    //@BindView(R.id.nameEditText) EditText mNameEditText;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  mNameEditText = (EditText) findViewById(R.id.nameEditText);
-     //   mFindCryptosButton = (Button)findViewById(R.id.findCryptosButton);
-          //  mFindCryptosButton.setOnClickListener(new View.OnClickListener() {
-          //
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUserActivity.class));
+                break;
+        }
+
     }
 }
