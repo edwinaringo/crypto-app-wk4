53e6c19cd1f3562da97a674d6dbbf05e87508d65
edwinaringo <tedaringo628@gmail.com>
2021-07-19 23:21:28 +0300

    Creating if statements for validating user inputs

--- app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
+++ app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
@@ -2,7 +2,9 @@ package com.example.cryptoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,6 +46,56 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
 
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Full name is required!");
+            editTextFullName.requestFocus();
+            return;
+        }
+        if(age.isEmpty()){
+            editTextAge.setError("Age is required!");
+            editTextAge.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Min password length should be 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
     }
 }
