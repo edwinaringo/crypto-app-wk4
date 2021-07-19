1e8fb835b67aad69b13ee54d9f6c9c04c0795451
edwinaringo <tedaringo628@gmail.com>
2021-07-19 23:11:08 +0300

    Created all the variables that we need ti register new user

--- app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
+++ app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
@@ -3,12 +3,27 @@ package com.example.cryptoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class RegisterUserActivity extends AppCompatActivity {
+public class RegisterUserActivity extends AppCompatActivity{
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user_activity);
     }
+
 }
