b8284e09cd76be66da8be6cbaefe2b1acd81c33f
edwinaringo <tedaringo628@gmail.com>
2021-07-19 23:13:06 +0300

    Initialized the variables

--- app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
+++ app/src/main/java/com/example/cryptoapp/RegisterUserActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class RegisterUserActivity extends AppCompatActivity{
+public class RegisterUserActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView banner, registerUser;
     private EditText editTextFullName, editTextAge, editTextEmail, editTextPassword;
@@ -24,6 +25,25 @@ public class RegisterUserActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user_activity);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
